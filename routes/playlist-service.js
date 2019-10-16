b11ca4d5afe92a085a804d89ba84e094a544a241
brian-thornton <psutron@gmail.com>
2019-10-16 17:22:45 -0400

    Playlist fix

--- routes/playlist-service.js
+++ routes/playlist-service.js
@@ -8,6 +8,10 @@ var appRouter = function (app, options) {
     res.status(200).send(listKeeper.getAllPlaylists());
   });
 
+  app.get("/playlists/getPlaylist", function (req, res) {
+    res.status(200).send(listKeeper.getPlaylist(req.query.name));
+  });
+
   app.post("/playlists/addToPlaylist", function (req, res) {
     res.status(200).send(listKeeper.addToPlaylist(req.body.name, req.body.tracks));
   });
