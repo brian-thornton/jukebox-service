3abe7388919446a02982b70bbea2e84dca2af3e7
Brian Thornton <Psutron@gmail.com>
2020-03-24 21:02:22 -0400

    Started spotify integration

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const librarianSerivce = require("./routes/librarian-service.js");
 const queueSerivce = require("./routes/queue-service.js");
 const volumeService = require("./routes/volume-service.js");
 const playlistService = require("./routes/playlist-service.js");
+const spotifyService = require("./routes/spotify-service.js");
 const app = express();
 
 app.use(bodyParser.json());
@@ -30,6 +31,7 @@ librarianSerivce(app, options);
 queueSerivce(app, options);
 volumeService(app, options);
 playlistService(app, options);
+spotifyService(app, options);
 
 librarian.getAll().forEach((library) => {
   if (!fs.existsSync(library.path)) {
