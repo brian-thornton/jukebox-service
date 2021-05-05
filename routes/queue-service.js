498fb351a8670abe47441b11efa019e9bdc7f4b4
Brian Thornton <brianthornton@brians-mbp.lan>
2021-05-05 06:08:05 -0400

    Updates for paging

--- routes/queue-service.js
+++ routes/queue-service.js
@@ -3,7 +3,7 @@ const JUtils = require('jukebox-utils');
 const appRouter = (app, options) => {
   const queue = new JUtils.queue(options);
 
-  app.get('/queue/getQueue', (req, res) => res.status(200).send(queue.getQueue()));
+  app.get('/queue/getQueue', (req, res) => res.status(200).send(queue.getQueue(req.query.start, req.query.limit)));
   app.get('/queue/enqueue', (req, res) => res.status(200).send(queue.enqueue(req.query.track)));
   app.post('/queue/clearQueue', (req, res) => res.status(200).send(queue.clearQueue()));
   app.get('/queue/play', (req, res) => res.status(200).send(queue.play()));
