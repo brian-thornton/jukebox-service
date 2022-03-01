c3111f140b3ccc37589e482a68c759841c7cf69f
Brian Thornton <brianthornton@brians-mbp.lan>
2022-02-28 19:29:07 -0500

    Added artHistory

--- routes/status-service.js
+++ routes/status-service.js
@@ -8,6 +8,12 @@ const appRouter = (app, options) => {
   app.post('/status/updateStatus', (req, res) => {
     res.status(200).send(status.updateStatus(req.body));
   });
+
+  app.get('/status/getArtHistory', (req, res) => res.status(200).send(status.getArtHistory()));
+
+  app.post('/status/updateArtHistory', (req, res) => {
+    res.status(200).send(status.updateArtHistory(req.body));
+  });
 };
 
 module.exports = appRouter;
